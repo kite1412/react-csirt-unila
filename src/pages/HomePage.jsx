a0fabf73cbad5681f4c5ea20f6e3f554b8b3000a
Tomy <tomyaryafiosa@gmail.com>
2025-06-06 14:59:34 +0700

    fix holographic card

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -182,23 +182,27 @@ function UnilaWebsites({ unilaSites }) {
   return (
     <div className="grid grid-cols-3 place-items-center p-10 text-white w-full gap-15 rounded-xl shadow-sm shadow-black/30">
       {unilaSites.map((unilaSite, index) => (
-        <a
-          key={unilaSite.name}
-          href={unilaSite.url}
-          target="_blank"
-          className="holographic-card w-full h-20 rounded-xl relative overflow-hidden shadow-sm shadow-black/100 flex justify-between items-center px-20"
-          style={{
-            background:
-              "linear-gradient(270deg,rgba(19, 104, 109, 1) 30%, rgba(8, 20, 35, 1) 100%)",
-          }}
+        <div
           data-aos="zoom-in"
-          data-aos-delay={index * 100}
+          data-aos-delay={index * 50}
+          className="w-full h-fit"
         >
-          <span className="font-semibold text-xl">
-            {unilaSite.name.toUpperCase()}
-          </span>
-          <LogoUnila className="w-15 h-auto" />
-        </a>
+          <a
+            key={unilaSite.name}
+            href={unilaSite.url}
+            target="_blank"
+            className="holographic-card w-full h-20 rounded-xl relative overflow-hidden shadow-sm shadow-black/100 flex justify-between items-center px-20 duration-200"
+            style={{
+              background:
+                "linear-gradient(270deg,rgba(19, 104, 109, 1) 30%, rgba(8, 20, 35, 1) 100%)",
+            }}
+          >
+            <span className="font-semibold text-xl">
+              {unilaSite.name.toUpperCase()}
+            </span>
+            <LogoUnila className="w-15 h-auto" />
+          </a>
+        </div>
       ))}
     </div>
   );
